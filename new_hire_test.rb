5130b9dfa0b2df4de7b066b97be602fbb1710f8d
Justin Valentini <justin.valentini@xls.xerox.com>
2013-11-06 13:48:03 -0500

    Create a github issue

--- new_hire_test.rb
+++ new_hire_test.rb
@@ -1,1 +1,4 @@
 require "github_api"
+
+github = Github::Issues.new oauth_token: 'ef3804aafc0c3097cb37ee03a0939396558d5ade', user: 'jvalentini', repo: 'new_hire_test'
+github.create title: "Test", body: "Bug description goes here"
